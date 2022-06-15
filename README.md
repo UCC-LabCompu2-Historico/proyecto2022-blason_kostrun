2eda9cdd7a553ce28f4d80dc7bdea1cb64a1e862
RoyKostrun <2116571@ucc.edu.ar>
2022-06-14 21:31:13 -0300

    cambios nuevos

--- README.md
+++ README.md
@@ -86,22 +86,22 @@ Link al sitio: [proyecto2022-Brick_Breaker](https://ucc-labcompu2.github.io/proy
 
 ### Sobre el proyecto general
 
-- [ ] Todas las correcciones y mejoras solicitadas durante el primer parcial deben estar corregidas.
-- [ ] No debe haber errores presentes en el código (realizar Code > Inspect Code para verificar que no haya errores)
-- [ ] No debe haber errores JavaScripts presentes. (ver con F12 si hay errores)
+- [X] Todas las correcciones y mejoras solicitadas durante el primer parcial deben estar corregidas.
+- [X] No debe haber errores presentes en el código (realizar Code > Inspect Code para verificar que no haya errores)
+- [X] No debe haber errores JavaScripts presentes. (ver con F12 si hay errores)
 
 ### Sobre la funcionalidad JavaScript
 
 Se debe agregar funcionalidad Js a la página HTML+CSS desarrollada
 
-- [ ] Una función que compruebe si los valores ingresados son correctos, y si no lo son, que le indique al usuario por
+- [X] Una función que compruebe si los valores ingresados son correctos, y si no lo son, que le indique al usuario por
   un alert, y que blanquee el contenido del campo.
-- [ ] Una función que calcule algo en base a los valores ingresados por el usuario en los inputs.
-- [ ] Una función que realice un dibujo sobre un canvas (debe ser representativo y acorde a los valores ingreados).
+- [] Una función que calcule algo en base a los valores ingresados por el usuario en los inputs.
+- [X] Una función que realice un dibujo sobre un canvas (debe ser representativo y acorde a los valores ingreados).
 
 ### Sobre la documentación
 
-- [ ] TODAS las funciones javaScript deben estar comentadas
+- [X] TODAS las funciones javaScript deben estar comentadas
   adecuadamente. [JsDoc](https://jsdoc.app/about-getting-started.html)
    ```/**
      * Descripción de que hace la función
--- game.js
+++ game.js
@@ -1,59 +1,64 @@
+//Para comenzar a dibujar, creamos una variable canvas a la cual hacemos referencia con el document.getElementById
+//Creamos la variable context ctx para poder acceder a todas los metodos particulares del canvas
 
-/** Para comenzar a dibujar, primero creamos una variable llamada canvas la cual le haciamos refeerencia con el document.getElement.Id
- *  Creamos la variable de context ctx para poder axeder a todas los metodos particulares del canvas
- *
- * **/
 var canvas = document.getElementById("lienzo");
 var ctx = canvas.getContext("2d");
-/**
- * Queremos dibujar la pelota.
- * Comenzamos el dibujo con un beginPath
- * Creamos dos variables x e y
- * **/
+
+
+
+
+//Al dibujar la bola creamos dos variables x e y, comenzamos el dibujo con beginPath
+
 var x = canvas.width/2;
 var y = canvas.height - 10;
 
-/**
- * Para el movimiento de la pelota, al x e y , deberiamos generar un diferencial
- * osea, agregar un pequeño x e y despues de que cada fotograma se haya dibujado para que represente que la pelota
- * se esta moviendo
- * Definimos dx y dy para modificar x e y
- * **/
-var dx = 2;
-var dy = -2; // -2 porque la pelota va de abajo hacia arriba
 
-/** Generamos un Objeto pelota
- *
- *  Agrego atributos, luego agrego un metodo( dibujar )
- * **/
+//Para lograr el movimiento de la bola, generamos un diferencial para x e y. Esto es agregar un pequeño x e y despues de
+// que cada fotograma se haya dibujado, para asi representar el movimiento de la bola. Entonces definimos un dx, dy
+// para modificar los valores originales x, y respectivamente.
+
+var dx = 2;
+var dy = -2;        // -2 porque la bola va de abajo hacia arriba
+
+
+//Generamos un objeto bola, agregamos atributos y un metodo (dibujar)
+
 var pelota = {
     radio: 10,
+
+    /**
+     * Dibuja la bola (asignamos color, tamaño)
+     * @method dibujar
+     *
+     */
+
     dibujar: function (){
         ctx.beginPath();
-        ctx.arc(x, y, this.radio,0 , 2*Math.PI); // puede llegar a tener 5 parametros, si yo quiero la pelota dibujada a la mitad de x y de y, this.radio es el radio declarado, y la peota completa de 0 a 2*Math.PI
-        ctx.fillStyle = "pink";// para asignarle un color
-        ctx.fill(); // pintamos la pelota
-        ctx.closePath(); // termino el path de dibujo
+        ctx.arc(x, y, this.radio,0 , 2*Math.PI); // puede llegar a tener 5 parametros, si yo quiero la pelota dibujada a la mitad de x y de y, this.radio es el radio declarado, y la pelota completa de 0 a 2*Math.PI
+        ctx.fillStyle = "pink";             // para asignarle un color
+        ctx.fill();                         // pintamos la pelota
+        ctx.closePath();                    // termino el path de dibujo
     }
 };
 
-
-/**
- * Movimiento de la Pelota
- *
- * canvas.width =canvas.width - Limpiamos el canvas
- * #Para que algo se dibuje repetitivamente cada cierto tiempo usamas al funcion setInterval()
- * **/
+//Movimiento de la bola
+//canvas.width =canvas.width --> Limpiamos el canvas
+//Para que algo se dibuje repetitivamente cada cierto tiempo usamos la función setInterval()
 
 
-/**
- * Creamos un objeto para la barra
- * **/
+//Creamos un objeto para la barra (tamaño, color, forma)
+
 
 var barra = {
     ancho: 75,
     alto: 10,
     posicionX: (canvas.width - 75) / 2,
+
+    /**
+     * Dibuja la barra con la que se dirige la bola(asignamos color, tamaño)
+     * @method dibujar
+     *
+     */
     dibujar: function (){
         ctx.beginPath();
         ctx.fillRect(this.posicionX, 450, this.ancho, this.alto); // dibujamos el rectangulo
@@ -63,26 +68,38 @@ var barra = {
     }
 };
 
+
+//rebote de la pelota, anidand if
+//Creamos condicion para el movimiento de la barra
+
 /**
+ * Se encarga de llamar a las demas funciones (para dibujar bola, barra, cuadro de ladrillos)
+ * @method dibujar
  *
- * Rebote de la pelota, anidand if
- *
- * Creamos condicion para el moviento de la barra
- * **/
-function  dibujar(){
+ */
+
+function dibujar(){
     canvas.width =canvas.width;
 
     if (x + dx < pelota.radio || x + dx > canvas.width-pelota.radio){
         dx =- dx;
     }
-    if (y + dy < pelota.radio || y + dy > canvas.height-pelota.radio) {
+    if (y + dy < pelota.radio) {
         dy =- dy;
+    }
+    if (y + dy > canvas.height-pelota.radio){
+        if(x > barra.posicionX && x < barra.ancho+barra.posicionX){
+            dy =-dy;
+        } else{
+            alert("Perdiste");
+        }
+
     }
 
     if (presionadoDerecho && barra.posicionX < canvas.width - barra.ancho){
-        barra.posicionX += 7; // para que se mueva a la derecha
+        barra.posicionX += 7;           // para que se mueva a la derecha
     } else if (presionadoIzquierdo && barra.posicionX > 0){
-        barra.posicionX -= 7; // para moverse a la izquierda
+        barra.posicionX -= 7;           // para moverse a la izquierda
     }
 
     pelota.dibujar();
@@ -94,30 +111,42 @@ function  dibujar(){
 }
 setInterval(dibujar, 10);
 
-/**
- *
- * Movemos la paleta
- * document.addEventListener - pulsado de teclas
- *
- * keyDownHandler - Al pulsar cualquier tecla la funcion se ejecutará
- * keyUpHandler - Caundo se libere el boton que se pulsó
- * **/
 
-var presionadoDerecho  = false; // para decir que no va a estar presionado inicialmente
+/**
+* Movemos la barra
+*document.addEvenListener - pulsado de teclas
+*keyDownHandler - Al pulsar cualquier tecla la funcion se ejecutará
+*keyUpHandler - cuando se libere el boton pulsado
+**/
+
+var presionadoDerecho  = false;          // para decir que no va a estar presionado inicialmente
 var presionadoIzquierdo = false;
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
+/**
+ *Ejecuta la funcion al pulsar una tecla
+ *@method keyDownHandler
+ *@param {//tipo de parametro} event-
+ */
+
 function keyDownHandler(event){
-    if (event.keyCode === 39){ // verificamos boton derecho cuyo keycode es 39
+    if (event.keyCode === 39){           // verificamos boton derecho cuyo keycode es 39
         presionadoDerecho = true;
-    } else if ( event.keyCode === 37 ){ // verificamos boton izquierdo cuyo keycode es 39
+    } else if ( event.keyCode === 37 ){  // verificamos boton izquierdo cuyo keycode es 39
         presionadoIzquierdo = true;
     }
 }
 
 // para detectar cuando la tecla se presionó:
+
+/**
+ * Indica cuando la tecla ya no esta pulsada
+ * @method keyUpHandler
+ * @param event
+ */
+
 
 function keyUpHandler(event){
     if (event.keyCode === 39){
@@ -127,12 +156,7 @@ function keyUpHandler(event){
     }
 }
 
-/**
- *
- * -Definimos un objeto ladrillo.
- * -Cremos un especie de cuadro donde guardaremos las filas y columnas de ladrillos
- * -Dibujamos el cuadro
- * **/
+//Definimos un objeto ladrillo y dibujamos el cuadro donde guardaremos las filas y columnas de ladrillos
 
 var ladrillo = {
     alto: 20,
@@ -144,7 +168,7 @@ var espacio = 10;
 var espacioDerecho = 30;
 var espadioIzquierdo = 30;
 
-// pasamos por las filas / columnas y creamos los ladrillos
+//Pasamos por las filas / columnas y creamos los ladrillos
 var cuadroDLadrillos = [];
 for (i = 0; i < columnas ; i++){
     cuadroDLadrillos[i] = [];
@@ -153,6 +177,12 @@ for (i = 0; i < columnas ; i++){
     }
 }
 
+
+/**
+ * Dibujamos el cuadro para los ladrillos
+ * @method dibujarCuadroDeLadrillos
+ *
+ */
 function dibujarCuadroDeLadrillos() {
     for (i = 0; i < columnas; i++) {
         for (j = 0; j < filas; j++) {
@@ -167,3 +197,6 @@ function dibujarCuadroDeLadrillos() {
         }
     }
 }
+
+
+
